ea40775cd49ae614ff7e689004b718a1d941227d
Filippo Gioia <filippo.gioia@gmail.com>
2022-10-16 11:18:49 +0100

    renamed and tidied up components

--- src/components/fizzbuzz.jsx
+++ src/components/fizzbuzz.jsx
@@ -1,102 +1,109 @@
 import React, { Component } from "react";
-import handleFizz from "./handleFizz";
+import handleFizz from "./handlers";
 
 class Fizzbuzz extends Component {
-  state = {
-    number: Math.floor(Math.random() * 100 + 1),
-    message: "",
-    count: 0,
-  };
+    state = {
+        number: Math.floor(Math.random() * 100 + 1),
+        message: "",
+        count: 0,
+    };
 
-  handleRefresh = () => {
-    //re-roll number
-    this.setState({ number: Math.floor(Math.random() * 100 + 1) });
-  };
+    handleRefresh = () => {
+        //re-roll number
+        this.setState({ number: Math.floor(Math.random() * 100 + 1) });
+    };
 
-  handleFizzbuzz = () => {
-    if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
-      this.setState({ message: "Correct" });
-    if (this.state.number % 3 !== 0 && this.state.number % 5 !== 0)
-      this.setState({ message: "Incorrect" });
-    if (this.state.number % 3 === 0 && this.state.number % 5 !== 0)
-      this.setState({ message: "Incorrect" });
-    if (this.state.number % 5 === 0 && this.state.number % 3 !== 0)
-      this.setState({ message: "Incorrect" });
-  };
+    handleFizzbuzz = () => {
+        if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
+            this.setState({ message: "Correct" });
+        if (this.state.number % 3 !== 0 && this.state.number % 5 !== 0)
+            this.setState({ message: "Incorrect" });
+        if (this.state.number % 3 === 0 && this.state.number % 5 !== 0)
+            this.setState({ message: "Incorrect" });
+        if (this.state.number % 5 === 0 && this.state.number % 3 !== 0)
+            this.setState({ message: "Incorrect" });
+    };
 
-  //   handleFizz = () => {
-  //     if (this.state.number % 3 === 0) this.setState({ message: "Correct" });
-  //     if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
-  //       this.setState({ message: "This is also divisible by 5!" });
-  //     if (this.state.number % 3 !== 0) this.setState({ message: "Incorrect" });
-  //   };
+    //   handleFizz = () => {
+    //     if (this.state.number % 3 === 0) this.setState({ message: "Correct" });
+    //     if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
+    //       this.setState({ message: "This is also divisible by 5!" });
+    //     if (this.state.number % 3 !== 0) this.setState({ message: "Incorrect" });
+    //   };
 
-  buttonHandleFizz = () => {
-    this.setState({ message: handleFizz(this.state.number) });
-  };
+    buttonHandleFizz = () => {
+        this.setState({ message: handleFizz(this.state.number) });
+        console.log(this.state.message, 'before', this.state.count);
+        this.updateCounter();
+        console.log(this.state.message, 'after', this.state.count);
+    };
 
-  handleBuzz = () => {
-    if (this.state.number % 5 === 0) this.setState({ message: "Correct" });
-    if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
-      this.setState({ message: "This is also divisible by 3!" });
-    if (this.state.number % 5 !== 0) this.setState({ message: "Incorrect" });
-  };
 
-  handlePass = () => {
-    if (this.state.number % 3 !== 0 && this.state.number % 5 !== 0)
-      this.setState({ message: "Correct" });
-    if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
-      this.setState({ message: "Incorrect" });
-    if (this.state.number % 3 === 0 && this.state.number % 5 !== 0)
-      this.setState({ message: "Incorrect" });
-    if (this.state.number % 3 !== 0 && this.state.number % 5 === 0)
-      this.setState({ message: "Incorrect" });
-  };
+    handleBuzz = () => {
+        if (this.state.number % 5 === 0) this.setState({ message: "Correct" });
+        if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
+            this.setState({ message: "This is also divisible by 3!" });
+        if (this.state.number % 5 !== 0) this.setState({ message: "Incorrect" });
+    };
 
-  updateCounter = () => {
-    if (this.state.message === "Correct")
-      this.setState({ count: this.state.count + 1 });
-  };
+    handlePass = () => {
+        if (this.state.number % 3 !== 0 && this.state.number % 5 !== 0)
+            this.setState({ message: "Correct" });
+        if (this.state.number % 3 === 0 && this.state.number % 5 === 0)
+            this.setState({ message: "Incorrect" });
+        if (this.state.number % 3 === 0 && this.state.number % 5 !== 0)
+            this.setState({ message: "Incorrect" });
+        if (this.state.number % 3 !== 0 && this.state.number % 5 === 0)
+            this.setState({ message: "Incorrect" });
+    };
 
-  render() {
-    return (
-      <React.Fragment>
-        <span style={{ fontSize: 15 }} className="badge bg-danger m-3">
-          {this.state.count}
-        </span>
-        <br />
-        <button
-          onClick={this.handleRefresh}
-          className="btn btn btn-warning m-3"
-        >
-          Refresh
-        </button>
-        <span style={{ fontSize: 50 }} className="badge bg-primary m-3">
-          {this.state.number}
-        </span>
-        <button
-          onClick={this.buttonHandleFizz}
-          className="btn btn btn-secondary m-3"
-        >
-          Fizz
-        </button>
-        <button onClick={this.handleBuzz} className="btn btn btn-secondary m-3">
-          Buzz
-        </button>
-        <button
-          onClick={this.handleFizzbuzz}
-          className="btn btn btn-secondary m-3"
-        >
-          Fizzbuzz
-        </button>
-        <button onClick={this.handlePass} className="btn btn btn-secondary m-3">
-          Pass
-        </button>
-        <br />
-        {this.state.message}
-      </React.Fragment>
-    );
-  }
+    updateCounter = () => {
+        if (this.state.message === "Correct")
+            this.setState({ count: this.state.count + 1 });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span style={{ fontSize: 15 }} className="badge bg-danger m-3">
+                    {this.state.count}
+                </span>
+                <br />
+                <button
+                    onClick={this.handleRefresh}
+                    className="btn btn btn-warning m-3"
+                >
+                    Refresh
+                </button>
+                <span style={{ fontSize: 50 }} className="badge bg-primary m-3">
+                    {this.state.number}
+                </span>
+                <button
+                    onClick={this.buttonHandleFizz}
+                    className="btn btn btn-secondary m-3"
+                >
+                    Fizz
+                </button>
+                <button
+                    onClick={this.handleBuzz}
+                    className="btn btn btn-secondary m-3"
+                >
+                    Buzz
+                </button>
+                <button
+                    onClick={this.handleFizzbuzz}
+                    className="btn btn btn-secondary m-3"
+                >
+                    Fizzbuzz
+                </button>
+                <button onClick={this.handlePass} className="btn btn btn-secondary m-3">
+                    Pass
+                </button>
+                <br />
+                {this.state.message}
+            </React.Fragment>
+        );
+    }
 }
 
 export default Fizzbuzz;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Fizzbuzz from "./components/fizzbuzz";
-// import Counter from "./components/counter";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Counter /> */}
     <Fizzbuzz />
   </React.StrictMode>
 );
